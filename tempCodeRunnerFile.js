3b9216a87823beb006c2e6faafc4fb0cc58742c6
naphattar <naphatchalermwai@gmail.com>
2023-11-21 01:37:15 +0700

    add data.json

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,6 @@
-use('db-final-project')
-db.religiousplaces.find({})
+use('db-final-project');
+
+db.religiousplaces.find({
+    religiousType: "Jedi",
+    $expr: { $gte: [{ $size: "$priest_list" }, 2] }
+});
